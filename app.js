7415ec5f153a4a540d1c61b8f43f5cf158f94269
Jen Tam <jennifer.k.tam@gmail.com>
2017-04-28 18:54:10 -0700

    Added /router, CSS, HTML, and express static

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const tweets = require('./tweetBank.js');
 
 // MIDDLEWARE
 
 // CONSOLE LOGGING MIDDLEWARE
+app.use(express.static('public'));
 app.use(morgan('dev'));
+app.use('/', routes);
 
-app.get('/', function(req, res) {
-  console.log('hi');
-  res.send('You have reached the GET / route!');
-});
-
-app.get('/news', function(req, res) {
-  res.send('This is the /news route');
-});
+// NUNJUCKS TEMPLATING ENGINE INTEGRATION
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCach: true}); // point nunjucks to the proper directory for templates
 
 app.listen(3000, function() {
-  console.log("You have successfully reached 3000!");
+  console.log("You have successfully reached port 3000!");
 })
